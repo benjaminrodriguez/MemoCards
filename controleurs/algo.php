f5abae6541adaca14c41a0ae5425632403279aa6
Reybun <reybun@users.noreply.github.com>
2018-11-19 16:21:26 +0100

    Update algo.php

--- controleurs/algo.php
+++ controleurs/algo.php
@@ -1,1 +1,44 @@
-giup
+<?php
+
+if ($answer === true)
+{
+    $level_card++;
+    $chain++;
+}
+else if ($answer === false)
+{
+    $level_card++;
+    $chain = 0;
+}
+if ($chain === 3) {
+    $level_card--;
+    $chain = 0;
+}
+
+if ($cpt < $nb_card) 
+{
+    $choice = rand(0, 100);
+    
+    if ($choice < 35) 
+    {
+        include("./modeles/quest1.php");
+    }
+    else    
+    {
+        include("./modeles/quest2.php");
+    }
+    
+    if ($cpt === 0) {
+        $list += '\''.$IDDELAQUESTION.'\'';
+
+    }
+    else 
+    {
+        $list += ', \''.$IDDELAQUESTION.'\'';
+    }
+    
+    $cpt++;
+}
+
+
+?>
