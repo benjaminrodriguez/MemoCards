f18436ff882fb222a2e15544ff4fd520597fec55
benjaminrodriguez <benjamin-rodriguez@outlook.fr>
2018-11-19 14:58:41 +0100

    dbconnect

---
+++ modeles/req_sql.php
@@ -0,0 +1,11 @@
+<?php
+        //CONNEXION A LA BDD
+        try
+        {
+                $bdd = new PDO('mysql:host=localhost;dbname=MemoCards;charset=utf8', 'root', 'toor');
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+?>
