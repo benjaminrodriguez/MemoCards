f4a38088194465688f8e1c1f2cbce2d1fd5e3b7a
benjaminrodriguez <benjamin-rodriguez@outlook.fr>
2018-11-19 14:06:59 +0100

    test debug

---
+++ controleurs/accueil_controleurs.php
@@ -0,0 +1,38 @@
+<?php
+
+  if (!isset($_SESSION['username']))
+  {
+    //header('Location: index.php?page=connexion');
+    //exit;
+    echo "string";
+  } else {
+    echo "gggggg";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //include(dirname(_FILE_).'/../vues/index_vues.php');
+
+?>
