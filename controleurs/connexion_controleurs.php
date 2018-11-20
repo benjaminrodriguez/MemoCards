57bcd85b12eecec9285602f53707c11feae984b5
benjaminrodriguez <benjamin-rodriguez@outlook.fr>
2018-11-20 09:22:14 +0100

    debut insctiption connexion

--- controleurs/connexion_controleurs.php
+++ controleurs/connexion_controleurs.php
@@ -1,3 +1,15 @@
 <?php
-  include(dirname(__FILE__).'/../vues/connexion_vues.html');
+  if (!isset($_SESSION['username']))
+  {
+    include(dirname(__FILE__).'/../vues/connexion_vues.html');
+  }
+  if (!empty($_POST['username']) && !empty($_POST['password']))
+  {
+    if (isset($_POST['username']) && isset($_POST['password']))
+    { 
+      $_SESSION['username'] = $_POST['username'];
+      $_SESSION['password'] = $_POST['password'];
+      include("./verifyPseudoPass.php");
+    }
+  }
 ?>
