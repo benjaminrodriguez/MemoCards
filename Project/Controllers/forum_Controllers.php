2957a3e56f31eda5de34cb69aceeca5d893c8032
Benjamin Rodriguez <benjamin-rodriguez@outlook.fr>
2018-12-05 20:00:13 +0100

    NO HEROKU, CAPTCHA

--- Project/Controllers/forum_Controllers.php
+++ Project/Controllers/forum_Controllers.php
@@ -1,9 +1,10 @@
 <?php    
+
     // HEADER
     require(dirname(__FILE__).'/../Views/top_menu_Views.php');
 
     // CREER SUJET
-    if (!isset($_GET['id'])) 
+    if (!isset($_GET['subject_id'])) 
     {
 
         // AFFICHE TOUS LES SUJETS DU FORUM
@@ -25,30 +26,58 @@
             echo 'vous n\'avez pas les droits';
         }
     }
-    
-    if (isset($_GET['id'])) 
+
+    if (isset($_GET['subject_id'])) 
     {
-        $_SESSION['subject_id'] = intval($_GET['id']);
+        $_SESSION['subject_id'] = intval($_GET['subject_id']);
+
         // ON ECRIT MESSAGE DANS SUJET
         require_once(dirname(__FILE__).'/php/write_topic.php');
 
         // AFFICHE LE CONTENU DU SUJET
-        first_messages_subject_SELECT($_GET['id']);
+        first_messages_subject_SELECT($_GET['subject_id']);
 
         // AFFICHE MESSAGES D'UN SUJET
-        $print_message = messages_subject_SELECT(intval($_GET['id']));
+        $print_message = messages_subject_SELECT(intval($_GET['subject_id']));
         foreach ($print_message as $key => $value) 
         {
             require(dirname(__FILE__).'/../Views/forum_affichage_message.php');
+        }
+        var_dump($_GET);
+        var_dump($_SESSION);
+
+        if (isset($_GET['subject_id']))
+        {
+            $_SESSION['subject_id'] = intval($_GET['subject_id']);
         }
     
     }
     
     // SUPPRIMER UN MESSAGE DONT ON EST L'AUTEUR
-    if (isset($_POST['choix_forum']) && $_POST['choix_forum'] == 'delete_message') 
+    if (isset($_GET['choix_forum']) && $_GET['choix_forum'] == 'delete_message')
     {
-        echo 'coucou';
-        require_once(dirname(__FILE__).'/php/delete_message.php');
+        var_dump($_GET);
+
+        //$_SESSION['message_num'] = intval($_GET['message_num']);
+        if (isset($_SESSION['subject_id']))
+        {
+            var_dump($_GET);
+
+            // AFFICHER AUTEUR D'UN MESSAGE
+            $subject_info = messages_autor_SELECT($_SESSION['subject_id'], $_GET['message_num']);
+            foreach ($subject_info as $key => $value) 
+            {
+                if ($subject_info[$key]['autor_id'] == $_SESSION['id'])
+                {
+                    message_DELETE($_GET['message_num'], $_SESSION['subject_id']);
+                }
+                else 
+                {
+                    echo 'desole seul l\'auteur peut delete son message';
+
+                }
+            }
+        }
     }
 
     require(dirname(__FILE__).'/../Views/forum_Views.php');
